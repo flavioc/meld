00050178b8a173e79bf526b76c1dd9823544a614
Flávio Cruz <flaviocruz@gmail.com>
2011-05-14 16:25:33 +0100

    Add bounded priority queue.

---
+++ sched/queue/bounded_pqueue.hpp
@@ -0,0 +1,130 @@
+
+#ifndef SCHED_QUEUE_BOUNDED_PQUEUE_HPP
+#define SCHED_QUEUE_BOUNDED_PQUEUE_HPP
+
+#include <vector>
+
+#include "sched/queue/safe_queue.hpp"
+#include "utils/atomic.hpp"
+
+namespace sched
+{
+   
+template <class C> // parameter is a container
+class queue_tree_node
+{
+public:
+   typedef queue_tree_node<C> tree_node;
+   
+   utils::atomic<int> counter;
+   tree_node *parent, *left, *right;
+   C bin;
+   
+   inline const bool is_leaf(void) const { return right == NULL; }
+   
+   explicit queue_tree_node(void):
+      counter(0),
+      parent(NULL),
+      left(NULL),
+      right(NULL)
+   {
+   }
+   
+   ~queue_tree_node(void)
+   {
+      assert(counter == 0);
+   }
+};
+   
+template <class T>
+class bounded_pqueue
+{
+private:
+   
+   typedef queue_tree_node< safe_queue<T> > tree_node;
+   
+   std::vector<tree_node*> leaves;
+   tree_node *root;
+   
+   utils::atomic<size_t> total;
+   
+   tree_node* build_tree(const size_t height, const size_t prio)
+   {
+      tree_node *root = new tree_node();
+      if(height == 0) {
+         assert(prio < leaves.size());
+         leaves[prio] = root;
+      } else {
+         root->left = build_tree(height - 1, 2 * prio);
+         root->right = build_tree(height - 1, (2 * prio) + 1);
+         root->left->parent = root->right->parent = root;
+      }
+      
+      return root;
+   }
+   
+   
+public:
+   
+   inline const bool empty(void) const { return total == 0; }
+   
+   void push(const T item, const size_t prio)
+   {
+      tree_node *node = leaves[prio];
+      node->bin.push(item);
+      
+      while(node != root) {
+         tree_node *parent = node->parent;
+         if(node == parent->left) // increment if ascending from left
+            parent->counter++;
+         node = parent;
+      }
+      
+      total++; // has more tasks
+   }
+   
+   T pop(void)
+   {
+      assert(!empty());
+      
+      tree_node *node = root;
+      
+      while(!node->is_leaf()) {
+         if(node->counter-- > 0)
+            node = node->left;
+         else
+            node = node->right;
+      }
+      
+      T ret(node->bin.pop());
+      
+      total--;
+      
+      return ret;
+   }
+   
+   explicit bounded_pqueue(const size_t range):
+      total(0)
+   {
+      assert(range > 0);
+      
+      const size_t number_leaves = utils::next_power2(range);
+      
+      assert(number_leaves >= range);
+      
+      leaves.resize(number_leaves);
+      
+      root = build_tree(utils::upper_log2(number_leaves), 0);
+   }
+   
+   ~bounded_pqueue(void)
+   {
+      assert(root != NULL);
+      assert(empty());
+   }
+};
+
+}
+
+#endif
+
