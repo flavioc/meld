893a43535b706371bdf76bd0af005104f4d42221
Flávio Cruz <flaviocruz@gmail.com>
2011-05-20 02:54:48 +0100

    Add mpi, mix.

--- benchs/benchmark.sh
+++ benchs/benchmark.sh
@@ -24,13 +24,15 @@ time_run ()
 
 time_run_n ()
 {
-	EXEC="${1}"
+	CMD="${1}"
 	DESC="${2}"
 
 	echo -n "${DESC} "
+
+	local time=()
 	
 	for((I = 0; I < ${RUNS}; I++)); do
-		time[${I}]="$(time_run ${EXEC})"
+		time[${I}]="$(time_run ${CMD})"
 	done
 
 	total=0
@@ -56,6 +58,25 @@ do_time_thread ()
 	time_run_n "${TO_RUN}" "$(basename ${FILE} .m) ${SCHEDULER} ${THREADS} threads"
 }
 
+do_time_mpi ()
+{
+	FILE="${1}"
+	PROCS="${2}"
+
+	TO_RUN="mpirun -np ${PROCS} ${EXEC} ${FILE} -c mpi"
+	time_run_n "${TO_RUN}" "$(basename ${FILE} .m) mpi ${PROCS} processes"
+}
+
+do_time_mix ()
+{
+	FILE="${1}"
+	PROCS="${2}"
+	THREADS="${3}"
+
+	TO_RUN="mpirun -np ${PROCS} ${EXEC} ${FILE} -c mix${THREADS}"
+	time_run_n "${TO_RUN}" "$(basename ${FILE} .m) mix ${PROCS}/${THREADS}"
+}
+
 time_thread ()
 {
 	FILE="${1}"
@@ -65,6 +86,66 @@ time_thread ()
 	do_time_thread ${FILE} 4
 	do_time_thread ${FILE} 8
 	do_time_thread ${FILE} 16
+}
+
+time_mpi ()
+{
+	FILE="${1}"
+
+	do_time_mpi ${FILE} 1
+	do_time_mpi ${FILE} 2
+	do_time_mpi ${FILE} 4
+	do_time_mpi ${FILE} 8
+	do_time_mpi ${FILE} 16
+}
+
+time_mix ()
+{
+	FILE="${1}"
+	
+	do_time_mix ${FILE} 1 1
+	do_time_mix ${FILE} 1 2
+	do_time_mix ${FILE} 2 1
+	do_time_mix ${FILE} 4 1
+	do_time_mix ${FILE} 1 4
+	do_time_mix ${FILE} 2 2
+	do_time_mix ${FILE} 6 1
+	do_time_mix ${FILE} 1 6
+	do_time_mix ${FILE} 3 2
+	do_time_mix ${FILE} 2 3
+	do_time_mix ${FILE} 8 1
+	do_time_mix ${FILE} 1 8
+	do_time_mix ${FILE} 4 2
+	do_time_mix ${FILE} 2 4
+	do_time_mix ${FILE} 10 1
+	do_time_mix ${FILE} 1 10
+	do_time_mix ${FILE} 5 2
+	do_time_mix ${FILE} 2 5
+	do_time_mix ${FILE} 12 1
+	do_time_mix ${FILE} 1 12
+	do_time_mix ${FILE} 2 6
+	do_time_mix ${FILE} 6 2
+	do_time_mix ${FILE} 3 4
+	do_time_mix ${FILE} 4 3
+	do_time_mix ${FILE} 14 1
+	do_time_mix ${FILE} 1 14
+	do_time_mix ${FILE} 2 7
+	do_time_mix ${FILE} 7 2
+	do_time_mix ${FILE} 16 1
+	do_time_mix ${FILE} 1 16
+	do_time_mix ${FILE} 2 8
+	do_time_mix ${FILE} 8 2
+	do_time_mix ${FILE} 4 4
+	do_time_mix ${FILE} 18 1
+	do_time_mix ${FILE} 1 18
+	do_time_mix ${FILE} 9 2
+	do_time_mix ${FILE} 2 9
+	do_time_mix ${FILE} 3 6
+	do_time_mix ${FILE} 6 3
+	do_time_mix ${FILE} 20 1
+	do_time_mix ${FILE} 1 20
+	do_time_mix ${FILE} 10 2
+	do_time_mix ${FILE} 2 10
 }
 
 time_file ()
