484e89598a2d2d5f1fcb719160d1018fe62f8887
Flávio Cruz <flaviocruz@gmail.com>
2011-08-01 17:15:24 +0100

    Use the same design for the multi queue.

--- sched/queue/safe_queue_multi.hpp
+++ sched/queue/safe_queue_multi.hpp
@@ -2,12 +2,10 @@
 #ifndef SCHED_QUEUE_SAFE_QUEUE_MULTI_HPP
 #define SCHED_QUEUE_SAFE_QUEUE_MULTI_HPP
 
-#include <boost/thread/mutex.hpp>
-#include <queue>
-
 #include "conf.hpp"
 #include "utils/spinlock.hpp"
 #include "utils/atomic.hpp"
+#include "sched/queue/node.hpp"
 
 namespace sched
 {
@@ -17,11 +15,28 @@ class safe_queue_multi
 {
 private:
    
-   std::queue<T> cont;
+   typedef queue_node<T> node;
+   
+   volatile node *head;
+   volatile node *tail;
 	utils::spinlock mtx;
+	
 #ifdef INSTRUMENTATION
    utils::atomic<size_t> total;
 #endif
+
+   inline void push_node(node *new_node)
+   {
+#ifdef INSTRUMENTATION
+      total++;
+#endif
+      assert(tail != NULL);
+      assert(head != NULL);
+
+      tail->next = new_node;
+      tail = new_node;
+      assert(tail->next == NULL);
+   }
    
 public:
    
@@ -29,49 +44,72 @@ public:
    inline size_t size(void) const { return total; }
 #endif
    
-   inline bool empty(void) const { return cont.empty(); }
+   inline bool empty(void) const { return head == tail; }
    
    inline bool pop(T& data)
    {
-      if(cont.empty())
-         return false;
-         
-		utils::spinlock::scoped_lock l(mtx);
+      assert(tail != NULL);
+      assert(head != NULL);
       
-      if(cont.empty())
+      if(empty())
          return false;
       
-      data = cont.front();
+      utils::spinlock::scoped_lock l(mtx);
       
-      cont.pop();
+      if(empty())
+         return false;
       
+      assert(head->next != NULL);
+      assert(!empty());
+      
+      node *take((node*)head->next);
+      node *old((node*)head);
+      
+      head = take;
+      
+      delete old;
+      
+      assert(head == take);
+      assert(take != NULL);
+      
+      data = take->data;
+
 #ifdef INSTRUMENTATION
       total--;
 #endif
-      
       return true;
    }
    
    inline void push(T data)
    {
-#ifdef INSTRUMENTATION
-      total++;
-#endif
+      node *new_node(new node());
+      
+      new_node->data = data;
+      new_node->next = NULL;
+      
 		utils::spinlock::scoped_lock l(mtx);
    
-      cont.push(data);
+      push_node(new_node);
    }
    
-   explicit safe_queue_multi(void)
+   explicit safe_queue_multi(void):
+      tail(NULL)
 #ifdef INSTRUMENTATION
-      : total(0)
+      , total(0)
 #endif
    {
+      head = new node();
+      head->next = NULL;
+      tail = head;
    }
    
    ~safe_queue_multi(void)
    {
       assert(empty());
+#ifdef INSTRUMENTATION
+      assert(total == 0);
+#endif
+      delete head;
    }
 };
    
