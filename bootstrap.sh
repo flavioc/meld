83886cb975607c4cdf0f3410e77f41aa23b23d77
Flávio Cruz <flaviocruz@gmail.com>
2012-05-28 20:23:47 -0400

    Update README (for installation) and remove Makefile.bak after bootstrapping.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,4 +6,5 @@
 cp Makefile.in Makefile
 echo "Finding dependencies..."
 makedepend -- -- `find . -name '*.cpp'` > /dev/null 2>&1
+rm -f Makefile.bak
 echo "Makefile built - type 'make' to compile project"
