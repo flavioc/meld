42ad9f7c8de41d55af7281a26681db3ce03e2c57
Flávio Cruz <flaviocruz@gmail.com>
2012-07-19 19:13:15 +0200

    Forgot to add these files.

---
+++ sched/serial.hpp
@@ -0,0 +1,67 @@
+
+#ifndef SCHED_SERIAL_HPP
+#define SCHED_SERIAL_HPP
+
+#include "sched/base.hpp"
+#include "sched/nodes/serial.hpp"
+#include "queue/unsafe_double_queue.hpp"
+#include "queue/intrusive.hpp"
+
+namespace sched
+{
+   
+class serial_local: public sched::base
+{
+protected:
+	
+	serial_node *current_node;
+	queue::intrusive_unsafe_double_queue<serial_node> queue_nodes;
+	
+private:
+   
+   inline bool has_work(void) const { return !queue_nodes.empty(); }
+   
+   virtual void assert_end(void) const;
+   virtual void assert_end_iteration(void) const;
+   virtual void generate_aggs(void);
+   
+public:
+   
+   virtual void new_agg(process::work&);
+   virtual void new_work(const db::node *, process::work&);
+   
+   virtual void new_work_other(sched::base *, process::work&)
+   {
+      assert(false);
+   }
+   
+   virtual void new_work_remote(process::remote *, const db::node::node_id, message *)
+   {
+      assert(false);
+   }
+   
+   virtual bool get_work(process::work&);
+   
+   virtual void init(const size_t);
+   virtual void end(void) {}
+   virtual bool terminate_iteration(void);
+   
+   static db::node* create_node(const db::node::node_id id, const db::node::node_id trans)
+   {
+      return dynamic_cast<db::node*>(new sched::serial_node(id, trans));
+   }
+   
+   serial_local *find_scheduler(const db::node *) { return this; }
+   
+   explicit serial_local(void):
+      sched::base(0),
+      current_node(NULL)
+   {
+   }
+   
+   virtual ~serial_local(void) {}
+};
+
+}
+
+#endif
---
+++ sched/serial_ui.hpp
@@ -0,0 +1,61 @@
+
+#ifndef SCHED_SERIAL_UI_HPP
+#define SCHED_SERIAL_UI_HPP
+
+#include "conf.hpp"
+
+#include "sched/base.hpp"
+#include "sched/nodes/serial.hpp"
+#include "sched/serial.hpp"
+
+#ifdef USE_UI
+#include <json_spirit.h>
+#endif
+
+namespace sched
+{
+   
+class serial_ui_local: public serial_local
+{
+private:
+   
+	bool first_done;
+	
+public:
+
+	virtual void new_persistent_derivation(db::node *, vm::tuple *);
+	virtual void new_linear_derivation(db::node *, vm::tuple *);
+	virtual void new_linear_consumption(db::node *, vm::tuple *);
+	
+	virtual void new_work(const db::node *, process::work&);
+	virtual bool get_work(process::work&);
+	
+	virtual void init(const size_t);
+	virtual void end(void);
+	
+	virtual void set_node_color(db::node *, const int, const int, const int);
+	virtual void set_edge_label(db::node *, const db::node::node_id, const runtime::rstring::ptr);
+   
+   static db::node* create_node(const db::node::node_id id, const db::node::node_id trans)
+   {
+      return dynamic_cast<db::node*>(new sched::serial_node(id, trans));
+   }
+
+#ifdef USE_UI
+	json_spirit::Value dump_this_node(const db::node::node_id);
+	static json_spirit::Value dump_node(const db::node::node_id);
+#endif
+
+	void change_to_node(serial_node*);
+	static void change_node(const db::node::node_id);
+
+   serial_ui_local *find_scheduler(const db::node *) { return this; }
+   
+	explicit serial_ui_local(void);
+   
+   virtual ~serial_ui_local(void) {}
+};
+
+}
+
+#endif
