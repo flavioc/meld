ac066634b6f0afb3bd79086d8e5c52d128b918a6
Flávio Cruz <flaviocruz@gmail.com>
2012-07-19 11:06:47 +0200

    Socket class.

---
+++ sim/socket.cpp
@@ -0,0 +1,56 @@
+
+#include <vector>
+
+#include "sim/socket.hpp"
+
+using namespace std;
+using boost::asio::ip::udp;
+
+namespace sim
+{
+	
+socket::socket(const string& host, const string& port):
+	s(NULL), receiver(NULL)
+{
+	boost::asio::io_service io_service;
+	
+	udp::resolver resolver(io_service);
+	udp::resolver::query query(udp::v4(), host.c_str(), port.c_str());
+	
+	receiver = new udp::endpoint(*resolver.resolve(query));
+	
+	s = new udp::socket(io_service);
+	s->open(udp::v4());
+}
+
+void
+socket::send(const message_buf data, const size_t len)
+{
+	std::vector<utils::byte> vec;
+	
+	for(size_t i(0); i < len; ++i)
+		vec.push_back(data[i]);
+	
+	s->send_to(boost::asio::buffer(vec), *receiver);
+}
+
+message_buf
+socket::receive(void)
+{
+	boost::array<utils::byte, MESSAGE_SIZE_BYTES> buf;
+	
+	udp::endpoint sender_endpoint;
+	size_t len(s->receive_from(boost::asio::buffer(buf), sender_endpoint));
+	
+	assert(len == MESSAGE_SIZE_BYTES);
+	
+	message_buf msg(new utils::byte[MESSAGE_SIZE_BYTES]);
+	
+	for(size_t i(0); i < MESSAGE_SIZE_BYTES; ++i) {
+		msg[i] = buf[i];
+	}
+	
+	return msg;
+}
+
+}
---
+++ sim/socket.hpp
@@ -0,0 +1,42 @@
+
+#ifndef SIM_SOCKET_HPP
+#define SIM_SOCKET_HPP
+
+#include <boost/asio.hpp>
+#include <string>
+
+#include "utils/types.hpp"
+
+#define MESSAGE_SIZE_BYTES 32
+
+namespace sim
+{
+	
+typedef utils::byte* message_buf;
+
+class socket
+{
+private:
+	
+	boost::asio::ip::udp::socket *s;
+	boost::asio::ip::udp::endpoint *receiver;
+	
+public:
+	
+	void send(const message_buf, const size_t);
+	message_buf receive(void);
+	
+	explicit socket(const std::string&, const std::string&);
+	
+	~socket(void) {
+		assert(s);
+		delete s;
+		assert(receiver);
+		delete receiver;
+	}
+};
+
+}
+
+#endif
+
