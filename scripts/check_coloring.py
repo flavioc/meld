cbb96e927600c39a4fb8c91ee3c84c9155bb9823
Flávio Cruz <flaviocruz@gmail.com>
2012-08-28 20:37:20 -0400

    Fix graph coloring. Add scripts to check graph coloring results.

---
+++ scripts/check_coloring.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+import sys
+from lib_db import read_db
+
+db = read_db(sys.stdin.readlines())
+
+neighbors = {}
+
+def add_neighbor(src, dst):
+	try:
+		mine = neighbors[src]
+		if dst in mine:
+			return
+		else:
+			mine.append(dst)
+			try:
+				other = neighbors[dst]
+				other.append(src)
+			except KeyError:
+			 	neighbors[dst] = [src]
+	except KeyError:
+		neighbors[src] = [dst]
+		try:
+			other = neighbors[dst]
+			other.append(src)
+		except KeyError:
+			neighbors[dst] = [src]
+
+def print_neighbors():
+	for src, dsts in neighbors.iteritems():
+		print src, dsts
+
+# add neighbors
+for node, data in db.iteritems():
+	for d in data:
+		name = d['name']
+		args = d['args']
+		if name == 'edge':
+			add_neighbor(node, int(args[0].lstrip('@')))
+
+colors = {}
+# add colors
+for node, data in db.iteritems():
+	checked = False
+	for d in data:
+		name = d['name']
+		if name == 'color':
+			checked = True
+			args = d['args']
+			color = int(args[0])
+			try:
+				older = colors[node]
+				print "Color already set for node", node
+				sys.exit(1)
+			except:
+				colors[node] = color
+				try:
+					edges = neighbors[node]
+					for edge in edges:
+						try:
+							other = colors[edge]
+							if other == color:
+								print "Node " + str(node) + " has same color as " + str(edge)
+								sys.exit(1)
+						except:
+							pass
+				except:
+					# All fine
+					pass
+	if checked is False:
+		print "No color was set for node", node
+		sys.exit(1)
+
+print "ALL OK"
---
+++ scripts/lib_db.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#
+# This library reads a VM output and builds its own database.
+
+def read_db(lines):
+	current_node = None
+	nodes = {}
+	node_db = []
+	for line in lines:
+		line = line.rstrip('\n')
+		if line.isdigit():
+			if current_node is not None:
+				nodes[current_node] = node_db
+			current_node = int(line)
+			node_db = []
+		else:
+			split1 = line.split('(')
+			split2 = split1[1].split(')')
+			name = split1[0]
+			args = split2[0].split(', ')
+			if len(args) == 1 and args[0] == '':
+				args = []
+			data = {'name': name, 'args': args}
+			node_db.append(data)
+	return nodes
