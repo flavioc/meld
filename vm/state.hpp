54cd03ed8ff93846a9941cc7e33787f8b8779b25
Flávio Cruz <flaviocruz@gmail.com>
2012-11-16 19:16:00 -0500

    Use tuple_field for registers instead of all_val.

--- vm/state.hpp
+++ vm/state.hpp
@@ -36,7 +36,7 @@ class state
 private:
    
    static const size_t NUM_REGS = 32;
-   typedef all_val reg;
+   typedef tuple_field reg;
    reg regs[NUM_REGS];
    db::tuple_trie_leaf *saved_leafs[NUM_REGS];
 	db::simple_tuple *saved_stuples[NUM_REGS];
@@ -142,32 +142,32 @@ public:
    inline RET get_ ## WHAT (const reg_num& num) const { BODY; }
    
    define_get(reg, reg, return regs[num]);
-   define_get(int, int_val, return *(int_val*)(regs + num));
-   define_get(float, float_val, return *(float_val*)(regs + num));
-   define_get(ptr, ptr_val, return *(ptr_val*)(regs + num));
+   define_get(int, int_val, return regs[num].int_field);
+   define_get(float, float_val, return regs[num].float_field);
+   define_get(ptr, ptr_val, return regs[num].ptr_field);
    define_get(bool, bool_val, return get_int(num) ? true : false);
 	define_get(string, runtime::rstring::ptr, return (runtime::rstring::ptr)get_ptr(num););
    define_get(int_list, runtime::int_list*, return (runtime::int_list*)get_ptr(num));
    define_get(float_list, runtime::float_list*, return (runtime::float_list*)get_ptr(num));
    define_get(node_list, runtime::node_list*, return (runtime::node_list*)get_ptr(num));
    define_get(tuple, vm::tuple*, return (vm::tuple*)get_ptr(num));
-   define_get(node, vm::node_val, return *(node_val *)(regs + num));
+   define_get(node, vm::node_val, return regs[num].node_field);
    
 #undef define_get
 
 #define define_set(WHAT, ARG, BODY) 												\
    inline void set_ ## WHAT (const reg_num& num, ARG val) { BODY; };
    
-   define_set(float, const float_val&, *(float_val*)(regs + num) = val);
-   define_set(int, const int_val&, *(int_val*)(regs + num) = val);
-   define_set(ptr, const ptr_val&, *(ptr_val*)(regs + num) = val);
+   define_set(float, const float_val&, regs[num].float_field = val);
+   define_set(int, const int_val&, regs[num].int_field = val);
+   define_set(ptr, const ptr_val&, regs[num].ptr_field = val);
    define_set(bool, const bool_val&, set_int(num, val ? 1 : 0));
 	define_set(string, const runtime::rstring::ptr, set_ptr(num, (ptr_val)val));
    define_set(int_list, runtime::int_list*, set_ptr(num, (ptr_val)val));
    define_set(float_list, runtime::float_list*, set_ptr(num, (ptr_val)val));
    define_set(node_list, runtime::node_list*, set_ptr(num, (ptr_val)val));
    define_set(tuple, vm::tuple*, set_ptr(num, (ptr_val)val));
-   define_set(node, const node_val, *(node_val*)(regs + num) = val);
+   define_set(node, const node_val, regs[num].node_field = val);
    
 #undef define_set
    
