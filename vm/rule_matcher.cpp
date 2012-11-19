d627291ceee8115e0d5b3d4b6d275ae7be91ecde
Flávio Cruz <flaviocruz@gmail.com>
2012-11-19 16:02:03 -0500

    Add missing files.

---
+++ vm/rule_matcher.cpp
@@ -0,0 +1,116 @@
+
+#include <algorithm>
+#include <iostream>
+
+#include "vm/rule_matcher.hpp"
+#include "vm/program.hpp"
+#include "vm/state.hpp"
+
+using namespace std;
+
+//#define DEBUG_RULES 1
+
+namespace vm
+{
+
+#ifdef USE_RULE_COUNTING
+
+void
+rule_matcher::register_predicate_availability(const predicate *pred)
+{
+	for(predicate::rule_iterator it(pred->begin_rules()), end(pred->end_rules()); it != end; it++) {
+		vm::rule_id rule(*it);
+
+      rules[rule].total_have++;
+		if(rules[rule].total_have == rules[rule].total_needed) {
+#ifdef DEBUG_RULES
+			cout << "Rule " << rule << " activated" << endl;
+#endif
+			active_rules.insert(rule);
+			dropped_rules.erase(rule);
+		}
+	}
+}
+
+void
+rule_matcher::register_predicate_unavailability(const predicate *pred)
+{
+	for(predicate::rule_iterator it(pred->begin_rules()), end(pred->end_rules()); it != end; it++) {
+		vm::rule_id rule(*it);
+
+		if(rules[rule].total_have == rules[rule].total_needed) {
+#ifdef DEBUG_RULES
+			cout << "Rule " << rule << " deactivated" << endl;
+#endif
+			active_rules.erase(rule);
+			dropped_rules.insert(rule);
+		}
+
+		rules[rule].total_have--;
+	}
+}
+
+/* returns true if we did not have any tuples of this predicate */
+bool
+rule_matcher::register_tuple(tuple *tpl, const ref_count count, const bool is_new)
+{
+   const vm::predicate_id id(tpl->get_predicate_id());
+	bool ret(false);
+#ifdef DEBUG_RULES
+	cout << "Add tuple " << *tpl << endl;
+#endif
+	if(is_new) {
+   	if(predicate_count[id] == 0) {
+			ret = true;
+      	register_predicate_availability(tpl->get_predicate());
+   	}
+	}
+
+   predicate_count[id] += count;
+	return ret;
+}
+
+/* returns true if now we do not have any tuples of this predicate */
+bool
+rule_matcher::deregister_tuple(tuple *tpl, const ref_count count)
+{
+   const vm::predicate_id id(tpl->get_predicate_id());
+	bool ret(false);
+	
+#ifdef DEBUG_RULES
+	cout << "Remove tuple " << *tpl << endl;
+#endif
+   assert(predicate_count[id] >= count);
+
+   if(predicate_count[id] == count) {
+		ret = true;
+		register_predicate_unavailability(tpl->get_predicate());
+   }
+
+   predicate_count[id] -= count;
+	return ret;
+}
+#endif
+
+rule_matcher::rule_matcher(void)
+{
+#ifdef USE_RULE_COUNTING
+	predicate_count.resize(state::PROGRAM->num_predicates());
+	rules.resize(state::PROGRAM->num_rules());
+
+	fill(predicate_count.begin(), predicate_count.end(), 0);
+	
+	rule_id rid(0);
+	for(rule_vector::iterator it(rules.begin()), end(rules.end());
+		it != end;
+		it++, rid++)
+	{
+		rule_matcher_obj& obj(*it);
+		
+		obj.total_have = 0;
+		obj.total_needed = state::PROGRAM->get_rule(rid)->num_predicates();
+	}
+#endif
+}
+
+}
---
+++ vm/rule_matcher.hpp
@@ -0,0 +1,66 @@
+
+#ifndef VM_RULE_MATCHER_HPP
+#define VM_RULE_MATCHER_HPP
+
+#include <vector>
+#include <set>
+
+#include "vm/predicate.hpp"
+#include "vm/tuple.hpp"
+
+namespace vm
+{
+
+class rule_matcher
+{
+private:
+	
+#ifdef USE_RULE_COUNTING
+   class rule_matcher_obj
+   {
+      public:
+         size_t total_have;
+         size_t total_needed;
+   };
+
+	typedef std::vector<rule_matcher_obj> rule_vector;
+
+   rule_vector rules; /* availability statistics per rule */
+   std::vector<ref_count> predicate_count; /* number of tuples per predicate */
+
+	typedef std::set<rule_id, std::less<rule_id>, mem::allocator<rule_id> > set_rules;
+	set_rules active_rules; /* rules that *may* be derivable */
+	set_rules dropped_rules; /* any dropped rule */
+	
+	void register_predicate_availability(const predicate *);
+	void register_predicate_unavailability(const predicate *);
+#endif
+
+public:
+
+#ifdef USE_RULE_COUNTING
+   /* returns true if we did not have any tuples of this predicate */
+	bool register_tuple(tuple *, const ref_count, const bool is_new = true);
+
+	/* returns true if now we do not have any tuples of this predicate */
+	bool deregister_tuple(tuple *, const ref_count);
+
+	void clear_dropped_rules(void)
+	{
+		dropped_rules.clear();
+	}
+
+	typedef set_rules::const_iterator rule_iterator;
+	
+	rule_iterator begin_active_rules(void) const { return active_rules.begin(); }
+	rule_iterator end_active_rules(void) const { return active_rules.end(); }
+	rule_iterator begin_dropped_rules(void) const { return dropped_rules.begin(); }
+	rule_iterator end_dropped_rules(void) const { return dropped_rules.end(); }
+#endif
+
+   rule_matcher(void);
+};
+
+}
+
+#endif
