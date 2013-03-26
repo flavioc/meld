754d35c1012fe42c7b5a0cbee00f277449e2226d
Flávio Cruz <flaviocruz@gmail.com>
2013-03-26 15:00:04 -0400

    Update test code files.

--- tests/test_linear.sh
+++ tests/test_linear.sh
@@ -7,12 +7,13 @@ run_test ()
    ./test.sh code/$1.m $SCHED 1 || exit 1
 }
 
-run_test linear-comp1
 run_test linear-delete
 run_test linear-keep1
 run_test linear-keep2
 run_test linear-ref-use
 run_test linear-repeat
+run_test linear-comp1
+run_test pagerank-linear-sync
 run_test dfs
-run_test mfp
 run_test mwst
+run_test mfp
