d0178fb7eda4aff9a2768f7b10440f25a5457d97
Flávio Cruz <flaviocruz@gmail.com>
2013-06-15 16:12:37 +0100

    Added another graph coloring test. Added exclusion files to test suite.

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+WHAT="$1"
+EXCEPT_LIST="non_deterministic.exclude" #"$2"
 
-for file in code/*.m; do
-	./test.sh $file ${1} || exit 1
-done
+run_test () {
+   ./test.sh $1 $WHAT || (echo "TEST FAILED" && exit 1)
+}
+
+not_excluded () {
+   [[ -z `grep "^$1$" $EXCEPT_LIST` ]]
+}
+
+if [ -z "$EXCEPT_LIST" ]; then
+   for file in `ls code/*.m`; do
+      run_test $file
+   done
+else
+   for file in `ls code/*.m`; do
+      if not_excluded `basename $file .m`; then
+         run_test $file
+      fi
+   done
+fi
