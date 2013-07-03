284c0656ded5d7e52a1d955c6458d712f16f94e1
Flávio Cruz <flaviocruz@gmail.com>
2013-07-03 16:28:33 +0100

    Make transform_pagerank output graph lab files and meld files with edges with weights.

--- scripts/transform_pagerank.py
+++ scripts/transform_pagerank.py
@@ -12,16 +12,30 @@ if len(sys.argv) != 2:
 	print "usage: transform_pagerank.py <file>"
 	sys.exit(1)
 
+output_meld = True
 file = sys.argv[1]
 f = open(file, 'rb')
 
-def write_inout(i, o):
-	print "!output(@" + str(i) + ", @" + str(o) + ")."
-	print "!input(@" + str(o) + ", @" + str(i) + ")."
+if output_meld:
+	print "type input(node, node, float)."
+	print "type output(node, node, float)."
+	print
+
+def write_inout(i, o, w):
+	if output_meld:
+		print "!output(@" + str(i) + ", @" + str(o) + ", " + str(w) + ")."
+		print "!input(@" + str(o) + ", @" + str(i) + ", " + str(w) + ")."
+	else:
+		print str(i) + "\t" + str(o) + "\t" + str(w)
+
+edge_list = {}
 
 for line in f:
 	line = line.rstrip('\n')
 	vec = line.split(' ')
 	id = vec[0].rstrip(':')
-	for dest in vec[1:len(vec)-1]:
-		write_inout(id, dest)
+	size = len(vec)-2
+	if size > 0:
+		w = 1.0 / float(size)
+		for dest in vec[1:len(vec)-1]:
+			write_inout(id, dest, w)
