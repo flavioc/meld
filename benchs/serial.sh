2c30db3bf14c351960e312289754e7f69126d4cc
Flávio Cruz <flaviocruz@gmail.com>
2013-08-26 15:45:44 +0100

    Added script to run the sequential scheduler.

---
+++ benchs/serial.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+FILE="${1}"
+RUNS="${2}"
+
+if [ -z "${RUNS}" ]; then
+	RUNS=3
+fi
+
+if [ -z "$FILE" ]; then
+   echo "usage: serial.sh <file> [n-runs=3]"
+   exit 1
+fi
+
+source $PWD/lib/common.sh
+TO_RUN="${EXEC} ${FILE} -c sl -- ${MELD_ARGS}"
+time_run_n "${TO_RUN}" "$(basename ${FILE} .m) sl"
