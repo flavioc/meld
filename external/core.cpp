1c4eab4d01890723824da1cd02cf9397f487cd73
Flávio Cruz <flaviocruz@gmail.com>
2014-02-05 16:05:03 +0000

    If using USE_REAL_NODES get the immediate address.

--- external/core.cpp
+++ external/core.cpp
@@ -17,14 +17,22 @@ node_priority(EXTERNAL_ARG(id))
 {
    DECLARE_NODE(id);
    float_val ret(0.0);
+   node *n(NULL);
 
+#ifdef USE_REAL_NODES
+   n = (node*)id;
+#else
    if(prio_db != NULL) {
-      node *n(prio_db->find_node(id));
+      n = prio_db->find_node(id);
       assert(n != NULL);
-      thread_intrusive_node *tn((thread_intrusive_node *)n);
-
-      ret = tn->get_float_priority_level();
    }
+#endif
+
+   if(n) {
+      thread_intrusive_node *tn((thread_intrusive_node *)n);
+      ret = tn->get_float_priority_level();
+   } else
+      ret = 0.0;
 
    RETURN_FLOAT(ret);
 }
@@ -35,20 +43,20 @@ cpu_id(EXTERNAL_ARG(id))
    int_val ret(0);
    DECLARE_NODE(id);
 
-#if 0
-   sched::base *sched((sched::base*)pthread_getspecific(sched_key));
-
-   assert(sched != NULL);
-
-   ret = (int_val)sched->get_id();
-#endif
+   node *n(NULL);
+#ifdef USE_REAL_NODES
+   n = (node*)id;
+#else
    if(prio_db != NULL) {
-      node *n(prio_db->find_node(id));
+      n = prio_db->find_node(id);
       assert(n != NULL);
-      thread_intrusive_node *tn((thread_intrusive_node *)n);
-
-      ret = tn->get_owner()->get_id();
    }
+#endif
+
+   if(n)
+      ret = n->get_owner()->get_id();
+   else
+      ret = 0;
 
    RETURN_INT(ret);
 }
