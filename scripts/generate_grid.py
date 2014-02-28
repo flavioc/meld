b756bceeeb9c3fb486514ec098a13e90f53b07c3
Flávio Cruz <flaviocruz@gmail.com>
2014-02-28 17:01:39 +0000

    Improved grid generating script.

--- scripts/generate_grid.py
+++ scripts/generate_grid.py
@@ -5,15 +5,18 @@ import sys
 from lib import write_edge
 from lib import set_weight
 
+WRITE_COORD = True
+WRITE_INOUT = False
+
 if len(sys.argv) < 2:
 	print "Usage: generate_grid.py <num nodes> [weight]"
 	sys.exit(1)
 
 if len(sys.argv) == 3:
 	set_weight(int(sys.argv[2]))
-	print "type route edge(node, node, int)."
-else:
-	print "type route edge(node, node)."
+	#print "type route edge(node, node, int)."
+#else:
+#print "type route edge(node, node)."
 
 arg = sys.argv[1]
 lenvec = arg.split('@', 2)
@@ -25,17 +28,55 @@ else:
    width = int(lenvec[0])
    height = width
 
-def write_line(line):
-	for column in range(0, width):
-		id = line * width + column
-		if line < height-1:
+def write_conn(id1, id2, w):
+	global WRITE_INOUT
+	if WRITE_INOUT:
+		print "!output(@" + str(id1) + ", @" + str(id2) + ", " + str(w) + ")."
+		print "!input(@" + str(id2) + ", @" + str(id1) + ", " + str(w) + ")."
+	else:
+		write_edge(id1, id2)
+
+def num_edges(row, col):
+	global width, height
+	if row == 0:
+		if col == 0:
+			return 2
+		elif col == height - 1:
+			return 2
+		else:
+			return 3
+	elif row == width - 1:
+		if col == 0:
+			return 2
+		elif col == height - 1:
+			return 2
+		else:
+			return 3
+	else:
+		if col == 0:
+			return 3
+		elif col == height - 1:
+			return 3
+		else:
+			return 4
+
+def write_line(row):
+	for col in range(0, width):
+		id = row * width + col
+		global WRITE_COORD
+		if WRITE_COORD:
+			print "!coord(@" + str(id) + ", " + str(row) + ", " + str(col) + ")."
+		edges_id = float(num_edges(row, col))
+		if row < height - 1:
 			southid = id + width
-			write_edge(southid, id)
-			write_edge(id, southid)
-		if column < width-1:
+			edges_southid = float(num_edges(row + 1, col))
+			write_conn(southid, id, 1.0 / edges_id)
+			write_conn(id, southid, 1.0 / edges_southid)
+		if col < width - 1:
 			eastid = id + 1
-			write_edge(eastid, id)
-			write_edge(id, eastid)
+			edges_eastid = float(num_edges(row, col + 1))
+			write_conn(eastid, id, 1.0 / edges_id)
+			write_conn(id, eastid, 1.0 / edges_eastid)
 			
-for line in range(0, height):
-	write_line(line)
+for row in range(0, height):
+	write_line(row)
