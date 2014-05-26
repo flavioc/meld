166d628ea670dc8f72e589ccf517116c3e67fe82
Flávio Cruz <flaviocruz@gmail.com>
2014-05-26 18:12:02 +0100

    Add memory tracking.

--- mem/center.cpp
+++ mem/center.cpp
@@ -8,6 +8,10 @@ extern boost::mutex allocator_mtx;
 extern std::tr1::unordered_set<void*> mem_set; 
 #endif
 
+#ifdef TRACK_MEMORY
+size_t bytes_used(0);
+#endif
+
 namespace mem
 {
 
@@ -17,6 +21,9 @@ center::allocate(size_t cnt, size_t sz)
    void *p;
    
    register_allocation(cnt, sz);
+#ifdef TRACK_MEMORY
+   bytes_used += cnt * sz;
+#endif
    
    if(USE_ALLOCATOR)
       p = get_pool()->allocate(cnt * sz);
@@ -44,6 +51,9 @@ center::deallocate(void *p, size_t cnt, size_t sz)
 #endif
 
    register_deallocation(cnt, sz);
+#ifdef TRACK_MEMORY
+   bytes_used -= cnt * sz;
+#endif
 
    if(USE_ALLOCATOR)
       get_pool()->deallocate(p, cnt * sz);
