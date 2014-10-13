811685609cb4cb5a8fcf305cb22c198d3621357d
Flávio Cruz <flaviocruz@gmail.com>
2014-10-13 12:33:54 -0400

    Disable indexing by default.

--- conf.mk
+++ conf.mk
@@ -5,7 +5,7 @@ EXTRA_ASSERTS = false
 # may use 'pool' or 'malloc'
 ALLOCATOR = pool
 # Use 'true' for dynamic indexing of facts.
-INDEXING = true
+INDEXING = false
 # Make the virtual machine use the node pointers in predicate arguments
 # typed as 'node'. For multithreaded meld this will improve speed since
 # there's one less lookup.
