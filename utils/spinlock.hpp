866990512f6f49d57d17e2e14e2ffa8abe5c6190
Flavio Cruz <flaviocruz@gmail.com>
2014-10-14 01:11:22 +0100

    Use the ticket spinlock as the spinlock algorithm.

--- utils/spinlock.hpp
+++ utils/spinlock.hpp
@@ -2,36 +2,62 @@
 #ifndef UTILS_SPINLOCK_HPP
 #define UTILS_SPINLOCK_HPP
 
-#include <atomic>
-
 namespace utils {
+
+typedef union ticketlock ticketlock;
+
+union ticketlock
+{
+   unsigned u;
+   struct
+   {
+      unsigned short ticket;
+      unsigned short users;
+   } s;
+};
 
 class spinlock
 {
    private:
-
-      std::atomic<bool> locked;
+      ticketlock t;
 
    public:
+      /* Compile read-write barrier */
+#define barrier() asm volatile("": : :"memory")
 
-      inline void lock(void) {
-         while(true) {
-            if(try_lock())
-               return;
-         }
+      /* Pause instruction to prevent excess processor bus usage */ 
+#define cpu_relax() asm volatile("pause\n": : :"memory")
+#define cmpxchg(P, O, N) __sync_val_compare_and_swap((P), (O), (N))
+#define atomic_xadd(P, V) __sync_fetch_and_add((P), (V))
+
+      inline bool try_lock(void)
+      {
+         unsigned short me = t.s.users;
+         unsigned short menew = me + 1;
+         unsigned cmp = ((unsigned) me << 16) + me;
+         unsigned cmpnew = ((unsigned) menew << 16) + me;
+
+         if (cmpxchg(&t.u, cmp, cmpnew) == cmp) return true;
+
+         return false;
       }
 
-      inline void unlock(void) {
-         locked.store(false, std::memory_order_release);
+      inline void unlock(void)
+      {
+         barrier();
+         t.s.ticket++;
       }
 
-      inline bool try_lock(void) {
-         bool expected(false);
-         return locked.compare_exchange_strong(expected, true, std::memory_order_acquire,
-               std::memory_order_relaxed);
+      inline void lock(void)
+      {
+         unsigned short me = atomic_xadd(&t.s.users, 1);
+
+         while (t.s.ticket != me) cpu_relax();
       }
 
-      explicit spinlock(void): locked(false) {}
+      explicit spinlock(void) {
+         t.u = 0;
+      }
 };
 
 }
