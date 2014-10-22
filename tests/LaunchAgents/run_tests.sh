40dc6f66dab32da0776a742459b50ea05a88cd65
Flávio Cruz <flaviocruz@gmail.com>
2014-10-22 04:25:43 -0400

    Remove old stuff.

--- tests/LaunchAgents/run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR/..
-date=`date`
-echo '===========================================' >> test_results
-echo "$date" >> test_results
-make test-serial 2>&1 >> test_results
