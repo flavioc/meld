2257578bd6d21911f97958c9e9298d430eec16bb
Flávio Cruz <flaviocruz@gmail.com>
2014-11-24 19:45:04 -0500

    Add missing file.

---
+++ utils/hash.hpp
@@ -0,0 +1,18 @@
+
+#ifndef UTILS_HASH_HPP
+#define UTILS_HASH_HPP
+
+namespace utils
+{
+
+template <typename T>
+struct pointer_hash {
+   size_t operator()(const T* val) const {
+      static const size_t shift((size_t)std::log2(1 + sizeof(T)));
+      return (size_t)(val) >> shift;
+   }
+};
+
+}
+
+#endif
