d4c5b85aafd85ebc4331d7b64a117cada12a12d5
Flávio Cruz <flaviocruz@gmail.com>
2015-03-11 16:35:24 +0000

    Improved script for snap.

--- scripts/stanford-snap.py
+++ scripts/stanford-snap.py
@@ -5,24 +5,38 @@
 #
 
 import sys
-from lib import write_edge
-from lib import set_weight
+import random
+from lib import write_edgew
 
 if len(sys.argv) < 2:
    print "Usage: stanford-snap.py <file> [distance]"
    sys.exit(1)
    
-if len(sys.argv) == 3:
-   set_weight(int(sys.argv[2]))
-else:
-   set_weight(1)
-   
 f = open(sys.argv[1], "rb")
+
+idmaps = {}
+newid = 0
+def map_id(id):
+   global newid, idmaps
+   try:
+      ret = idmaps[id]
+      return ret
+   except KeyError:
+      idmaps[id] = newid
+      newid = newid + 1
+      return newid - 1
 
 print "type route edge(node, node, int)."
 
 for line in f:
+   if not line:
+      continue
    if line[0] == '#':
       continue
-   vec = line.rstrip().split('\t')
-   write_edge(vec[0], vec[1])
+   vec = line.rstrip().split(' ')
+   id1 = map_id(int(vec[0]))
+   id2 = map_id(int(vec[1]))
+   if len(sys.argv) == 2:
+      write_edgew(id1, id2, int(random.randint(1, 20)))
+   else:
+      write_edgew(id1, id2, int(sys.argv[2]))
