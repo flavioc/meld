76cb232a41a34cc7b2e2de1706c6d2b7efe395bb
Flávio Cruz <flaviocruz@gmail.com>
2015-07-02 19:52:44 +0100

    Add script to uniformize snap files.

---
+++ scripts/uniform_snap.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# Uniformize ID's in a SNAP STANFORD file.
+
+import sys
+
+if len(sys.argv) != 2:
+   print "usage: uniform_snap.py <file>"
+   sys.exit(1)
+
+ids = {}
+count = 0
+for line in open(sys.argv[1], "r"):
+   line = line.rstrip().lstrip()
+   vec = line.split('\t')
+   if len(vec) != 2:
+      continue
+   id1 = int(vec[0])
+   id2 = int(vec[1])
+   try:
+      realid1 = ids[id1]
+   except KeyError:
+      realid1 = count
+      ids[id1] = realid1
+      count = count + 1
+   try:
+      realid2 = ids[id2]
+   except KeyError:
+      realid2 = count
+      ids[id2] = realid2
+      count = count + 1
+   print str(realid1) + "\t" + str(realid2)
