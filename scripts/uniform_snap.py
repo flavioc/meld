d461b91139b066fa4edca5a86f27a56596ce2176
Flavio Cruz <flaviocruz@gmail.com>
2015-07-03 15:31:24 +0100

    Add support for space separated edges.

--- scripts/uniform_snap.py
+++ scripts/uniform_snap.py
@@ -4,28 +4,30 @@
 import sys
 
 if len(sys.argv) != 2:
-   print "usage: uniform_snap.py <file>"
-   sys.exit(1)
+	print "usage: uniform_snap.py <file>"
+  	sys.exit(1)
 
 ids = {}
 count = 0
 for line in open(sys.argv[1], "r"):
-   line = line.rstrip().lstrip()
-   vec = line.split('\t')
-   if len(vec) != 2:
-      continue
-   id1 = int(vec[0])
-   id2 = int(vec[1])
-   try:
-      realid1 = ids[id1]
-   except KeyError:
-      realid1 = count
-      ids[id1] = realid1
-      count = count + 1
-   try:
-      realid2 = ids[id2]
-   except KeyError:
-      realid2 = count
-      ids[id2] = realid2
-      count = count + 1
-   print str(realid1) + "\t" + str(realid2)
+	line = line.rstrip().lstrip()
+	vec = line.split('\t')
+	if len(vec) == 1:
+		vec = line.split(' ')
+	if len(vec) != 2:
+		continue
+	id1 = int(vec[0])
+	id2 = int(vec[1])
+	try:
+		realid1 = ids[id1]
+	except KeyError:
+		realid1 = count
+		ids[id1] = realid1
+		count = count + 1
+	try:
+		realid2 = ids[id2]
+	except KeyError:
+		realid2 = count
+		ids[id2] = realid2
+		count = count + 1
+	print str(realid1) + "\t" + str(realid2)
