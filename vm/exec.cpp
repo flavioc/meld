4b8ba279ec430f5a74b09471f946645b337fe125
Flávio Cruz <flaviocruz@gmail.com>
2015-07-07 22:09:42 +0100

    Add needed argument.

--- vm/exec.cpp
+++ vm/exec.cpp
@@ -914,7 +914,7 @@ static inline return_type execute_linear_iter_list(
                // if this is the first iterate, we do not need to send this to
                // the generate list
                if (tbl)
-                  it = tbl->erase_from_list(db::hash_table::cast_list(local_tuples), it);
+                  it = tbl->erase_from_list(db::hash_table::cast_list(local_tuples), it, &(node->alloc));
                else
                   it = local_tuples->erase(it);
                state.add_generated(match_tuple, pred);
